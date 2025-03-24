0f9743a84a25b05c23baeb6ce8e2d4c2173ae6cd
Phil Bourg <phil@quaribou.net>
2025-03-23 23:52:48 -0400

    fix/prodFailedTooMuchAnimationLol:to test3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    // Reduce call stack usage during build
+    optimizePackageImports: [
+      "@heroicons/react",
+      "@headlessui/react",
+      "react-icons",
+    ],
+  },
+  // Explicitly define which files to include in the build
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  // Optimize image domains if you're using next/image
+  images: {
+    domains: [],
+  },
 };
 
 module.exports = nextConfig;
